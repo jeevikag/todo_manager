f248ada7bf7eec70f838d65eb41feacff346ca8b
jeevikag <jeevika1922@gmail.com>
2020-06-06 23:46:22 +0530

    Updated database migrations

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,17 @@
 class Todo < ActiveRecord::Base
-  def to_pleasant_string
-    is_completed = completed ? "[X]" : "[ ]"
-    "#{id}. #{due_date.to_s(:short)} #{todo_text} #{is_completed}"
+  def self.overdue
+    all.where("due_date < ? and completed= ?", Date.today, false)
+  end
+
+  def self.due_today
+    all.where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    all.where("due_date > ?", Date.today)
+  end
+
+  def self.completed
+    all.where(completed: true)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_03_152934) do
+ActiveRecord::Schema.define(version: 2020_06_06_155804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,11 +21,4 @@ ActiveRecord::Schema.define(version: 2020_06_03_152934) do
     t.boolean "completed"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 end
