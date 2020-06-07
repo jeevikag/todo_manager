b558bf9dd80427f2152198d1ab0739094c7fff2f
jeevikag <jeevika1922@gmail.com>
2020-06-07 16:25:50 +0530

    Added a form so that we can save a new todo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    def index
-        render plain: "Hello, this is /todos!"
-      end
+  def index
+    render plain: "Hello, this is /todos!"
+  end
 end
