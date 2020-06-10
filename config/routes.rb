6a236ef78d991bcf38296fd277fb9f8eed53e04a
jeevikag <jeevika1922@gmail.com>
2020-06-10 23:55:23 +0530

    Added sign-out functionality

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   get "/" => "home#index"
 
   resources :todos
-  resources :users
 
-  post "users/login", to: "users#login"
+  resources :users
 
   get "/signin" => "sessions#new", as: :new_sessions
   post "/signin" => "sessions#create", as: :sessions
+  delete "/signout" => "sessions#destroy", as: :destroy_session
 end
