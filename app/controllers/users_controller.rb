fdca9d7c944ddc22c1ade464c2dd53adc2070353
jeevikag <jeevika1922@gmail.com>
2020-06-10 18:02:41 +0530

    Used has_secure_password to save password_digest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,17 @@
 class UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  def index
-    render plain: User.order(:id).map { |user| user.to_pleasant_string }.join("\n")
+  def new
+    render "users/new"
   end
 
   def create
-    name = params[:name]
-    email = params[:email]
-    password = params[:password]
-    user = User.create!(
-      name: name,
-      email: email,
-      password: password,
+    User.create!(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      password: params[:password],
     )
-    response_text = "Hey, your new user is created with the id #{user.id}"
-    render plain: response_text
-  end
-
-  def login
-    email = params[:email]
-    password = params[:password]
-    user = User.find_by email: email
-    if (user != nil && user.password.to_s == password)
-      render plain: "true"
-    else
-      render plain: "false"
-    end
+    redirect_to "/"
   end
 end
